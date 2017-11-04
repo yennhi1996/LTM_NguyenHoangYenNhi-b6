d4cc8022239a251d324b7ded68260ea391c2d8cd
nataurus9x <nataurus9x@gmail.com>
2017-11-04 11:33:52 +0700

    B6

---
+++ Bai1-Client2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Bai1_Client2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            byte[] data = new byte[1024];
+            string stringData;
+            int recv;
+            sock.Connect(ipe);
+            Console.WriteLine("Connected to server:");
+            recv = sock.Receive(data);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine("Receive: {0}", stringData);
+
+            Console.Write("Nhap so luong phan tu: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap pt {0}: ", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            data = BitConverter.GetBytes(n);
+            sock.Send(data, sizeof(int), SocketFlags.None);
+            for (int i = 0; i < n; i++)
+            {
+                data = new byte[10];
+                data = BitConverter.GetBytes(array[i]);
+                sock.Send(data, sizeof(int), SocketFlags.None);
+            }
+            data = new byte[1024];
+            recv = sock.Receive(data);
+            int tong = BitConverter.ToInt32(data, 0);
+            Console.WriteLine("Tong: {0}", tong);
+
+            sock.Close();
+        }
+    }
+}
---
+++ Bai1-Server/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+
+namespace Bai1_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList sockList = new ArrayList(2);
+            ArrayList copyList = new ArrayList(2);
+            Socket main = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 9050); byte[] data = new byte[1024];
+            string stringData; int recv;
+            main.Bind(iep);
+            main.Listen(2);
+            Console.WriteLine("Waiting for 2 clients...");
+            Socket client1 = main.Accept();
+            IPEndPoint iep1 = (IPEndPoint)client1.RemoteEndPoint;
+            client1.Send(Encoding.ASCII.GetBytes("Welcome to my server"));
+            Console.WriteLine("Connected to {0}", iep1.ToString());
+            sockList.Add(client1); Console.WriteLine("Waiting for 1 more client...");
+            Socket client2 = main.Accept(); IPEndPoint iep2 = (IPEndPoint)client2.RemoteEndPoint;
+            client2.Send(Encoding.ASCII.GetBytes("Welcome to my server"));
+            Console.WriteLine("Connected to {0}", iep2.ToString());
+            sockList.Add(client2);
+            main.Close();
+            {
+                copyList = new ArrayList(sockList); Console.WriteLine("Monitoring {0} sockets...", copyList.Count);
+                Socket.Select(copyList, null, null, 10000000);
+                foreach (Socket client in copyList)
+                {
+                    data = new byte[1024];
+                    recv = client.Receive(data);
+                    int n = BitConverter.ToInt32(data, 0);
+                    Console.WriteLine("N = {0}", n);
+                    int[] array = new int[n];
+                    for(int i =0; i<n;i++)
+                    {
+                        data = new byte[10];
+                        recv = client.Receive(data);
+                        array[i] = BitConverter.ToInt32(data, 0);
+
+                    }
+                    int tong = 0;
+                    for(int i =0; i< n;i++)
+                    {
+                        tong += array[i];
+                    }
+                    data = new byte[10];
+                    data = BitConverter.GetBytes(tong);
+                    client.Send(data, sizeof(int), SocketFlags.None);
+                    client.Close();
+                }
+            }
+        }
+    }
+}
